9ee5846235080186b7d1628ba69952a9d43e5460
Gabriel Wicke <gwicke@wikimedia.org>
2017-05-31 14:08:57 -0700

    Relative paths for REST API
    - Consistently use /api/rest_v1/ to access the REST API. This makes MW
    work on random service IPs.
    - Configure RESTBase backend in VE settings.
    - Only expose port 80 from MW container. All the other ports are still
    reachable within the pod (via localhost), but should not be reachable
    externally.

--- conf/mediawiki/CustomSettings.php
+++ conf/mediawiki/CustomSettings.php
@@ -7,12 +7,9 @@ require_once "/usr/src/mediawiki/extensions/VisualEditor/VisualEditor.php";
 $wgDefaultUserOptions['visualeditor-enable'] = 1;
 $wgHiddenPrefs[] = 'visualeditor-enable';
 #$wgDefaultUserOptions['visualeditor-enable-experimental'] = 1;
-$wgVirtualRestConfig['modules']['parsoid'] = array(
-	// URL to the Parsoid instance
-	// Use port 8142 if you use the Debian package
-	'url' => 'http://mediawiki-node-services.docker:8142',
-	// Parsoid "domain", see below (optional)
+$wgVirtualRestConfig['modules']['restbase'] = array(
+	'url' => 'http://localhost:7231',
 	'domain' => 'localhost',
-	// Parsoid "prefix", see below (optional)
-	'prefix' => 'localhost'
+	'forwardCookies' => true,
+	'parsoidCompat' => false
 );
